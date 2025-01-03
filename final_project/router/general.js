3b4de33ce0140a8d3a0b276978a5224f2e0619ab
henrygoncalvess <octanebt@gmail.com>
2025-01-02 23:14:06 -0300

    retornando todos os livros, baseado em indicie e em autor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,20 +25,37 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.status(200).json(books)
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn
+
+        const bookDetails = await books[isbn]
+
+        res.status(200).json(bookDetails)
+    } catch (error) {
+        res.status(400)
+        throw error
+    }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const author = req.params.author
+
+        const bookAuthor = await Object.values(books).find(value => 
+            value.author === author
+        )
+
+        res.status(200).json(bookAuthor)
+    } catch (error) {
+        res.status(400)
+        throw error
+    }
 });
 
 // Get all books based on title
